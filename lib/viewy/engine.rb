fa3bae2330a8ebc7838d2c1ebb9a1e21ec06e206
Emerson Huitt <emerson.huitt@scimedsolutions.com>
2015-10-02 10:16:43 -0400

    No longer append migrations automatically

--- lib/viewy/engine.rb
+++ lib/viewy/engine.rb
@@ -1,13 +1,5 @@
 module Viewy
   class Engine < ::Rails::Engine
     isolate_namespace Viewy
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |path|
-          app.config.paths["db/migrate"] << path
-        end
-      end
-    end
   end
 end
